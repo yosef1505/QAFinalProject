33360fac86ecd1e9aacfa74d74efdf86bfba39c0
yosef <yaboqweder@gmail.com>
2025-01-29 15:49:36 +0200

    second

--- src/test/java/Tests/SanityTest.java
+++ src/test/java/Tests/SanityTest.java
@@ -10,14 +10,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-
 import static Pages.RegisterClass.*;
 import static Pages.SearchClass.*;
 import static org.testng.Assert.assertEquals;
